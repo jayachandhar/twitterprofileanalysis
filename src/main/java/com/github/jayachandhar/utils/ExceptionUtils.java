42481c8855c1ab38d283066744d9e512509b0f84
jayachandhar <jrj5046@gmail.com>
2018-08-10 23:13:02 +0530

    log format improvement

--- src/main/java/com/github/jayachandhar/utils/ExceptionUtils.java
+++ src/main/java/com/github/jayachandhar/utils/ExceptionUtils.java
@@ -9,15 +9,18 @@ public class ExceptionUtils {
         if (e.getClass() == TwitterException.class) {
             String reason = "";
             if (((TwitterException) e).getStatusCode() == 401) {
-                reason = "your account's private policy. Make sure that your account is open for public viewing and then retry.";
+                reason = "your account's private policy. Make sure that your account is open for public viewing " +
+                        "and then retry.";
             }
             if (((TwitterException) e).getStatusCode() == 404) {
-                reason = "No matching Account found for this screen name. please check your Twitter Screen Name and retry.";
+                reason = "No matching Account found for this screen name. please check your Twitter Screen Name" +
+                        " and retry.";
             }
             if (((TwitterException) e).getStatusCode() == 403) {
                 reason = "This screen name has been modified recently. please try with the new screen name.";
             }
-            return new ResponseEntity("Content not available.\nReason : " + reason, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity("Content not available." + (reason.equals("") ? "" : "\nReason : " + reason)
+                    , HttpStatus.BAD_REQUEST);
         } else
             return new ResponseEntity("Content not available try after some time.", HttpStatus.BAD_REQUEST);
 
