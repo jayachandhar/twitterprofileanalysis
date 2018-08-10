81d87d6d4998028dd502545374f08b74e6a611dc
jayachandhar <jrj5046@gmail.com>
2018-08-10 20:02:52 +0530

    log format improvement

--- src/main/java/com/github/jayachandhar/utils/ExceptionUtils.java
+++ src/main/java/com/github/jayachandhar/utils/ExceptionUtils.java
@@ -12,7 +12,10 @@ public class ExceptionUtils {
                 reason = "your account's private policy. Make sure that your account is open for public viewing and then retry.";
             }
             if (((TwitterException) e).getStatusCode() == 404) {
-                reason = "No matching Account found for this screen name. please check your Twitter Screen Name and retry";
+                reason = "No matching Account found for this screen name. please check your Twitter Screen Name and retry.";
+            }
+            if (((TwitterException) e).getStatusCode() == 403) {
+                reason = "This screen name has been modified recently. please try with the new screen name.";
             }
             return new ResponseEntity("Content not available.\nReason : " + reason, HttpStatus.BAD_REQUEST);
         } else
